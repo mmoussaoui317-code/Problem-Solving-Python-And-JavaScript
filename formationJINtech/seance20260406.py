0632177925c23c242c0be015c7b0bdca2c7ca41d
mmoussaoui317-code <mmoussaoui030107@gmail.com>
2026-04-13 21:15:33 +0100

    added note about python with some problems solving

--- formationJINtech/seance20260406.py
+++ formationJINtech/seance20260406.py
@@ -1,69 +1,69 @@
-# this seance leaning the list in python
+# # this seance leaning the list in python
 
-# list is []  
+# # list is []  
 
-# example 
-
-
-animals = ["cats", "dog"]
-
-print(animals[0])
-print(animals[1])
+# # example 
 
 
-animalsTwo = ["tiger", "owl"]
+# animals = ["cats", "dog"]
+
+# print(animals[0])
+# print(animals[1])
 
 
-print(animals + animalsTwo)
+# animalsTwo = ["tiger", "owl"]
 
 
-print(animalsTwo * 2)
+# print(animals + animalsTwo)
 
 
-
-print(dir(animalsTwo[0]))
-
-# del animalsTwo[0]
+# print(animalsTwo * 2)
 
 
 
-# print(animalsTwo.remove("tiger"))
+# print(dir(animalsTwo[0]))
 
-# print(animalsTwo)
-
-
-# pass by reference 
-
-
-# [operation axe condition]
-li_power = [i ** 2 for i in range(0, 5) if i > 0]
-
-print(li_power)
-
-
-# tuples 
-# can't changed """fixe"""
-
-fruits = ("strawberry", "water", "peach")
-
-
-# les function it's a block of code used multi times
-
-def hello_word():
-    return ("hello Coders!!!!")
-
-# call the function
-
-print(hello_word())
-
-
-print(help(print))
-
-
-# function with arguments 
-
-# give to it the inputs and after return to me the output
+# # del animalsTwo[0]
 
 
 
-# default values instead functions definition   (x, y, w = 0.0, r = 1.8)
+# # print(animalsTwo.remove("tiger"))
+
+# # print(animalsTwo)
+
+
+# # pass by reference 
+
+
+# # [operation axe condition]
+# li_power = [i ** 2 for i in range(0, 5) if i > 0]
+
+# print(li_power)
+
+
+# # tuples 
+# # can't changed """fixe"""
+
+# fruits = ("strawberry", "water", "peach")
+
+
+# # les function it's a block of code used multi times
+
+# def hello_word():
+#     return ("hello Coders!!!!")
+
+# # call the function
+
+# print(hello_word())
+
+
+# print(help(print))
+
+
+# # function with arguments 
+
+# # give to it the inputs and after return to me the output
+
+
+
+# # default values instead functions definition   (x, y, w = 0.0, r = 1.8)
